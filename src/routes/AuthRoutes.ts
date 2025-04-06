e1cc01ca7f4a125860000e8f1632ce75e20f045f
abdul-codes <abdulsalamakinsanya2@gmail.com>
2025-04-06 23:42:02 +0100

    Feat: Updated AuthRoutes

--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -1,13 +1,18 @@
 import { Router } from "express";
-import { loginValidation, registervalidation } from "../validation/validation";
+import { loginValidation, registervalidation, validateResendOtp, validateVeirfyOtp } from "../validation/validation";
 import { loginUser, logoutUser, registerUser } from "../controller/AuthController";
 import { authenticateUser } from "../middleware/authMIddleware";
+import { verifyOtp } from "../controller/verifyOtp";
+import { resendOtp } from "../controller/resendOtp";
 
 
 const router =  Router()
 
 router.post("/register", registervalidation, registerUser)
 router.post("/login", loginValidation, authenticateUser, loginUser)
+router.post("/verifyOtp", validateVeirfyOtp, verifyOtp)
+router.post("/resendOtp",validateResendOtp, resendOtp)
 router.post("/logout", logoutUser)
+
 
 export default router;
