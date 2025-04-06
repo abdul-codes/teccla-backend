fef4cebb5f8a80ed2466e2f2c4320d3a856771bf
abdul-codes <abdulsalamakinsanya2@gmail.com>
2025-04-06 23:44:09 +0100

    Fix: Updated Otps Validation

--- src/validation/validation.ts
+++ src/validation/validation.ts
@@ -3,35 +3,46 @@ import { NextFunction, Request, Response } from "express"
 import { asyncMiddleware } from "../middleware/asyncMiddleware"
 
 
-const handleValidationErrors = asyncMiddleware (async(req: Request, res: Response, next: NextFunction) => {
+const handleValidationErrors = asyncMiddleware(async (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-        return res.status(400).json({errors: errors.array() })
+        return res.status(400).json({ errors: errors.array() })
     }
     next()
 })
 
 
 export const registervalidation = [
-    body("email").isEmail().withMessage("Valid email is required"),
+    body("email").isEmail().notEmpty().withMessage("Valid email is required"),
     body("password")
-    .isLength({ min: 8 })
-    .withMessage('Password must be at least 8 characters long')
-    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
-    .withMessage('Password must include uppercase, lowercase, number, and special character'),
-  body('firstName').notEmpty().withMessage('First name is required'),
-  body('lastName').notEmpty().withMessage('Last name is required'),
-  handleValidationErrors
+        .isLength({ min: 8 })
+        .notEmpty()
+        .withMessage('Password must be at least 8 characters long')
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
+        .withMessage('Password must include uppercase, lowercase, number, and special character'),
+    body('firstName').notEmpty().withMessage('First name is required'),
+    body('lastName').notEmpty().withMessage('Last name is required'),
+    handleValidationErrors
 ]
 
 export const loginValidation = [
-    body("email").withMessage("email or accountId is required"),
+    body("accountId").notEmpty().withMessage("email or accountId is required"),
     body("password")
-    .isLength({ min: 8 }),
-    // .withMessage('Password must be at least 8 characters long')
+        .notEmpty()
+        .isLength({ min: 8 })
+        .withMessage('Password must be at least 8 characters long'),
     // .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
-   // .withMessage('Password must include uppercase, lowercase, number, and special character'),
-//   body('').notEmpty().withMessage('First name is required'),
-handleValidationErrors
+    // .withMessage('Password must include uppercase, lowercase, number, and special character'),
+    //   body('').notEmpty().withMessage('First name is required'),
+    handleValidationErrors
+]
+export const validateResendOtp = [
+    body('email').isEmail().normalizeEmail(),
+    handleValidationErrors
+]
+export const validateVeirfyOtp = [
+    body('email').isEmail().normalizeEmail(),
+    body('token').isInt().notEmpty(),
+    handleValidationErrors
 ]
 
