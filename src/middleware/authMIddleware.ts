7ff95b3a3b7f9129b3b24d344efa421d29f3569e
abdul-codes <abdulsalamakinsanya2@gmail.com>
2025-05-03 13:31:53 +0100

    Feat: Added Authroization roles

--- src/middleware/authMIddleware.ts
+++ src/middleware/authMIddleware.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { asyncMiddleware } from "./asyncMiddleware";
 import { prisma } from "../utils/db";
+import { UserRole } from "@prisma/client";
 
 
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN as string;
@@ -13,66 +14,66 @@ declare global {
       user?: {
         id: string;
         email: string;
-        role: string;
+        role: UserRole;
       }
     }
   }
 }
 
-export const authenticateUser = asyncMiddleware( async(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    try {
-      const authHeader = req.header("Authorization");
-      const token = req.header("Authorization")?.replace("Bearer ", "");
-  
-      if (!token) {
-        return res.status(401).json({
-          message: "No token, authorization denied",
-        });
-      }
-  
-      const decoded = jwt.verify(token, ACCESS_TOKEN) as {
-        id: string;
-        role: string;
-      };
-  
-      // Find user
-      const user = await prisma.user.findUnique({
-        where: { id: decoded.id },
-        select: {
-          id: true,
-          email: true,
-          role: true,
-        },
-      });
-  
-      // Check if user exists
-      if (!user) {
-        return res.status(401).json({
-          message: "User not found",
-        });
-      }
-  
-      // Attach user to request
-      req.user = user;
-  
-      next();
-    } catch (error) {
-      if (error instanceof jwt.JsonWebTokenError) {
-        res.status(401).json({ message: "Invalid or expired access token" });
-      } else {
-        console.error("Authentication error:", error);
-        res.status(500).json({ message: "Server error during authentication" });
-      }
-    }
-  });
-  
+export const authenticateUser = asyncMiddleware(async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const authHeader = req.header("Authorization");
+    const token = authHeader?.split(' ')[1];
 
-export const authorizeAdmin = asyncMiddleware( async(req: Request, res: Response, next: NextFunction) => {
-  if (req.user?.role !== 'admin') {
+    if (!token) {
+      return res.status(401).json({
+        message: "No token, authorization denied",
+      });
+    }
+
+    const decoded = jwt.verify(token, ACCESS_TOKEN) as {
+      id: string;
+      role: string;
+    };
+
+    // Find user
+    const user = await prisma.user.findUnique({
+      where: { id: decoded.id },
+      select: {
+        id: true,
+        email: true,
+        role: true,
+      },
+    });
+
+    // Check if user exists
+    if (!user) {
+      return res.status(401).json({
+        message: "User not found",
+      });
+    }
+
+    // Attach user to request
+    req.user = user;
+
+    next();
+  } catch (error) {
+    if (error instanceof jwt.JsonWebTokenError) {
+      res.status(401).json({ message: "Invalid or expired access token" });
+    } else {
+      console.error("Authentication error:", error);
+      res.status(500).json({ message: "Server error during authentication" });
+    }
+  }
+});
+
+
+export const authorizeAdmin = asyncMiddleware(async (req: Request, res: Response, next: NextFunction) => {
+  if (req.user?.role !== 'ADMIN') {
     return res.status(403).json({ message: 'Access denied. Admins only.' });
   }
   next();
@@ -80,13 +81,13 @@ export const authorizeAdmin = asyncMiddleware( async(req: Request, res: Response
 
 
 // Role-based Authorization Middleware
-export const authorizeRoles = (...roles: string[]) => {
-  return (req: Request, res: Response, next: Function) => {
-    if (!req.user || !roles.includes(req.user.role)) {
-      return res.status(403).json({ 
-        message: 'Access denied' 
+export const authorizeRoles = (...allowedroles: UserRole[]) => {
+  return asyncMiddleware(async(req: Request, res: Response, next: NextFunction) => {
+    if (!req.user || !allowedroles.includes(req.user.role)) {
+      return res.status(403).json({
+        message: 'Access denied'
       });
     }
     next();
-  };
+  });
 };
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { authenticateUser, authorizeAdmin } from "../middleware/authMIddleware";
+import { authenticateUser, authorizeAdmin, authorizeRoles } from "../middleware/authMIddleware";
 import { getAllUsers, updateCurrentUser } from "../controller/UserController";
+import { UserRole } from "@prisma/client";
 
 
 const router =  Router()
 
-router.get("/allusers",  getAllUsers)
+router.get("/allusers", authenticateUser, authorizeAdmin, getAllUsers)
+router.get("/allusers", authenticateUser, authorizeRoles(UserRole.ADMIN), getAllUsers)
 router.get("updateprofile", authenticateUser, updateCurrentUser)
 
 
