af5378c5ccb02e34f9facd84d4cff98673e3dbd5
abdul-codes <abdulsalamakinsanya2@gmail.com>
2025-11-19 03:35:36 +0100

    Refactor: Update port and add console message

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,29 @@
-import express, { Request, Response } from 'express'
-import cors from 'cors'
-import "dotenv/config"
-import compression from "compression"
-import limiter from './middleware/rateLImitMiddleware'
-import authRoutes from "./routes/AuthRoutes"
-import userRoutes from "./routes/UserRoutes"
-import userProfileRoutes from "./routes/UserProfileRoutes"
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import compression from "compression";
+import limiter from "./middleware/rateLImitMiddleware";
+import authRoutes from "./routes/AuthRoutes";
+import userRoutes from "./routes/UserRoutes";
+import userProfileRoutes from "./routes/UserProfileRoutes";
 
-const app = express()
+const app = express();
 
-app.use(compression())
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cors())
-app.use(limiter)
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(limiter);
 
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/profile", userProfileRoutes);
 
-app.use('/auth', authRoutes)
-app.use('/users', userRoutes)
-app.use('/profile', userProfileRoutes)
+app.get("/api/test", async (req: Request, res: Response) => {
+  res.json({ messsage: "hello and welcome back" });
+});
 
-//To use it only for a certain path (e.g., limit only calls to the /auth/* endpoints), 
-// specify the url as the first parameter in app.use
-// app.use('/auth', limiter)
-
-app.get("/api/test", async(req: Request, res: Response) => {
-    res.json({messsage: "hello and welcome back"})
-})
-
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
-    console.log("Server running on localhost:8000");
-    
-})
+  console.log(`Server running on localhost:${PORT}`);
+});
