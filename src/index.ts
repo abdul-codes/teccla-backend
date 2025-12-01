fd17184374788a701a402eb9870a2d773337ce0d
abdul-codes <abdulsalamakinsanya2@gmail.com>
2025-12-01 23:38:34 +0100

    Serve static files from uploads directory

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { createServer } from "http";
 import cors from "cors";
 import "dotenv/config";
 import compression from "compression";
+import path from "path";
 import limiter from "./middleware/rateLImitMiddleware";
 import authRoutes from "./routes/AuthRoutes";
 import userRoutes from "./routes/UserRoutes";
@@ -19,12 +20,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'http://localhost:5173' ,
   credentials: true, // Enable credentials (cookies, authorization headers, etc.)
   optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 app.use(cors(corsOptions));
-app.use(limiter);
+
+// Serve static files from uploads directory
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+
+// app.use(limiter);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users/profile", userProfileRoutes); // Must be BEFORE /api/users
@@ -44,5 +49,6 @@ initializeSocket(server);
 server.listen(PORT, () => {
   console.log(`Server running with Socket.io on localhost:${PORT}`);
 });
+// Trigger restart 2
 
 export default app;
