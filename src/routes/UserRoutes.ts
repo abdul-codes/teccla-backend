3013ab18ef3cdea471b7574ea57b010d30e5f67b
abdul-codes <abdulsalamakinsanya2@gmail.com>
2025-12-02 23:45:29 +0100

    Add search users route

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { authenticateUser, authorizeRoles } from "../middleware/authMIddleware";
-import { getAllUsers, getUserById, updateUser, deleteUser, getCurrentUser } from "../controller/UserController";
+import { getAllUsers, getUserById, updateUser, deleteUser, getCurrentUser, searchUsers } from "../controller/UserController";
 import { UserRole } from "@prisma/client";
 
 
@@ -11,6 +11,7 @@ router.get("/", authenticateUser, authorizeRoles(UserRole.ADMIN), getAllUsers)
 router.delete("/:id", authenticateUser, authorizeRoles(UserRole.ADMIN), deleteUser)
 
 // User routes (authenticated users)
+router.get("/search", authenticateUser, searchUsers) // Search users
 router.get("/profile", authenticateUser, getCurrentUser) // Get current user's profile
 router.get("/:id", authenticateUser, getUserById) // Get any user's profile by ID (for users to see others' profiles)
 router.put("/:id", authenticateUser, updateUser) // Update user profile by ID (users can update their own)
