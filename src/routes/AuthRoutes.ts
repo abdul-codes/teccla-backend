18a397b78aaf065288e0698bf35dbdc8238de27c
abdul-codes <abdulsalamakinsanya2@gmail.com>
2026-01-02 23:31:09 +0100

    Feat: Add forgot and reset password routes

--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { loginValidation, registervalidation, validateResendOtp, validateVerifyOtp } from "../validation/validation";
+import { loginValidation, registervalidation, validateResendOtp, validateVerifyOtp, forgotPasswordValidation, resetPasswordValidation } from "../validation/validation";
 import { loginUser, logoutUser, registerUser } from "../controller/AuthController";
 import { authenticateUser } from "../middleware/authMIddleware";
 import { verifyOtp } from "../controller/verifyOtp";
 import { resendOtp } from "../controller/resendOtp";
 import { refreshToken } from "../controller/JwtController";
+import { forgotPassword, resetPassword } from "../controller/PasswordResetController";
 
 
 const router =  Router()
@@ -14,6 +15,8 @@ router.post("/register", registervalidation, registerUser)
 router.post("/login", loginValidation, loginUser)
 router.post("/verifyOtp", validateVerifyOtp, verifyOtp)
 router.post("/resendOtp",validateResendOtp, resendOtp)
+router.post("/forgot-password", forgotPasswordValidation, forgotPassword)
+router.post("/reset-password", resetPasswordValidation, resetPassword)
 router.post("/logout", authenticateUser, logoutUser)
 
 export default router;
