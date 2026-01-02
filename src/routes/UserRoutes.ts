3b20ea006cbecb102c72a8a8ff4489c5884feb59
abdul-codes <abdulsalamakinsanya2@gmail.com>
2026-01-02 23:26:10 +0100

    Feat:Add endpoint to reset user lockout

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,19 +1,22 @@
 import { Router } from "express";
 import { authenticateUser, authorizeRoles } from "../middleware/authMIddleware";
-import { getAllUsers, getUserById, updateUser, deleteUser, getCurrentUser, searchUsers } from "../controller/UserController";
+import { getAllUsers, getUserById, updateUser, deleteUser, getCurrentUser, searchUsers, resetUserLockout } from "../controller/UserController";
+import { changePassword } from "../controller/PasswordChangeController";
 import { UserRole } from "../../prisma/generated/prisma/client";
-
+import { changePasswordValidation } from "../validation/validation";
 
 const router = Router()
 
 // Admin routes
 router.get("/", authenticateUser, authorizeRoles(UserRole.ADMIN), getAllUsers)
 router.delete("/:id", authenticateUser, authorizeRoles(UserRole.ADMIN), deleteUser)
+router.post("/:id/reset-lockout", authenticateUser, authorizeRoles(UserRole.ADMIN), resetUserLockout)
 
 // User routes (authenticated users)
-router.get("/search", authenticateUser, searchUsers) // Search users
-router.get("/profile", authenticateUser, getCurrentUser) // Get current user's profile
-router.get("/:id", authenticateUser, getUserById) // Get any user's profile by ID (for users to see others' profiles)
-router.put("/:id", authenticateUser, updateUser) // Update user profile by ID (users can update their own)
+router.get("/search", authenticateUser, searchUsers)
+router.get("/profile", authenticateUser, getCurrentUser)
+router.get("/:id", authenticateUser, getUserById)
+router.put("/:id", authenticateUser, updateUser)
+router.post("/change-password", authenticateUser, changePasswordValidation, changePassword)
 
 export default router;
