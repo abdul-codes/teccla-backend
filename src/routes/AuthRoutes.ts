8be46853da23ebd054a2587703d317cc315f2877
abdul-codes <abdulsalamakinsanya2@gmail.com>
2026-01-05 20:50:22 +0100

    Feat: Add rate limiting to auth routes

--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -1,22 +1,25 @@
 import { Router } from "express";
 import { loginValidation, registervalidation, validateResendOtp, validateVerifyOtp, forgotPasswordValidation, resetPasswordValidation } from "../validation/validation";
 import { loginUser, logoutUser, registerUser } from "../controller/AuthController";
-import { authenticateUser } from "../middleware/authMIddleware";
+import { authenticateUser } from "../middleware/authMiddleware";
 import { verifyOtp } from "../controller/verifyOtp";
 import { resendOtp } from "../controller/resendOtp";
 import { refreshToken } from "../controller/JwtController";
 import { forgotPassword, resetPassword } from "../controller/PasswordResetController";
 
 
-const router =  Router()
+import { authLimiter } from "../middleware/authRateLimitMiddleware";
+
+
+const router = Router()
 
 router.post("/refresh", refreshToken)
-router.post("/register", registervalidation, registerUser)
-router.post("/login", loginValidation, loginUser)
-router.post("/verifyOtp", validateVerifyOtp, verifyOtp)
-router.post("/resendOtp",validateResendOtp, resendOtp)
-router.post("/forgot-password", forgotPasswordValidation, forgotPassword)
-router.post("/reset-password", resetPasswordValidation, resetPassword)
+router.post("/register", authLimiter, registervalidation, registerUser)
+router.post("/login", authLimiter, loginValidation, loginUser)
+router.post("/verifyOtp", authLimiter, validateVerifyOtp, verifyOtp)
+router.post("/resendOtp", authLimiter, validateResendOtp, resendOtp)
+router.post("/forgot-password", authLimiter, forgotPasswordValidation, forgotPassword)
+router.post("/reset-password", authLimiter, resetPasswordValidation, resetPassword)
 router.post("/logout", authenticateUser, logoutUser)
 
 export default router;
